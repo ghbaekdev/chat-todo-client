da1535393dc90138f3b92af5bc2c4131c342523e
lukabaek <luka@twosun.com>
2023-02-06 09:19:32 +0900

    eslint 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,27 @@
-export module= {
-  "env": {
-    "browser": true,
-    "es2021": true,
-    "node": true
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
-  "extends": ["plugin:react/recommended", "standard"],
-  "overrides": [],
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module"
+  extends: ["plugin:react/recommended", "standard"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  "plugins": ["react"],
-  "rules": {},
+  plugins: ["react"],
+  rules: {},
   "import/no-anonymous-default-export": [
     {
-      "allowArray": false,
-      "allowArrowFunction": false,
-      "allowAnonymousClass": false,
-      "allowAnonymousFunction": true,
-      "allowCallExpression": true,
-      "allowNew": false,
-      "allowLiteral": false,
-      "allowObject": false
-    }
-  ]
-}
+      allowArray: false,
+      allowArrowFunction: false,
+      allowAnonymousClass: false,
+      allowAnonymousFunction: true,
+      allowCallExpression: true,
+      allowNew: false,
+      allowLiteral: false,
+      allowObject: false,
+    },
+  ],
+};
